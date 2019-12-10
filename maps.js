66e63c31cf903715982764b3347ecc97962518ae
Peter Han <peterjhan88@gmail.com>
2019-12-10 11:32:08 -0800

    minimum map creationg done

--- maps.js
+++ maps.js
@@ -7,6 +7,25 @@ function initializeApp() {
 var map;
 
 function createMap() {
+  var options = {
+    center: {
+      lat: 33.634984,
+      lng: -117.740424
+    },
+    zoom: 18,
+    disableDefaultUI: true,
+  };
+  map = new google.maps.Map(document.getElementById('map'), options);
+}
+$(document).ready(initializeApp);
+
+function initializeApp() {
+  $('#test-btn').on('click', createMap);
+}
+
+var map;
+
+function createMap() {
   console.log("creating map");
   var options = {
     center: {

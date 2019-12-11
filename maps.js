990311e45c58b7a1f69b582291d8582ad2aa445b
Peter Han <peterjhan88@gmail.com>
2019-12-11 10:09:02 -0800

    infowindow popup done Will show info-window popup done on LFZ for now

--- maps.js
+++ maps.js
@@ -4,26 +4,7 @@ function initializeApp() {
   $('#test-btn').on('click', createMap);
 }
 
-var map;
-
-function createMap() {
-  var options = {
-    center: {
-      lat: 33.634984,
-      lng: -117.740424
-    },
-    zoom: 18,
-    disableDefaultUI: true,
-  };
-  map = new google.maps.Map(document.getElementById('map'), options);
-}
-$(document).ready(initializeApp);
-
-function initializeApp() {
-  $('#test-btn').on('click', createMap);
-}
-
-var map;
+var map, infoWindow;
 
 function createMap() {
   console.log("creating map");
@@ -32,8 +13,16 @@ function createMap() {
       lat: 33.634984,
       lng: -117.740424
     },
-    zoom: 18,
-    disableDefaultUI: true
+    zoom: 15,
+    disableDefaultUI: true,
   };
   map = new google.maps.Map(document.getElementById('map'), options);
+  infoWindow = new google.maps.InfoWindow;
+  var position = {
+    lat: 33.634984,
+    lng: -117.740424
+  }
+  infoWindow.setPosition(position);
+  infoWindow.setContent('Learning FUZE!!!<br>Your location!');
+  infoWindow.open(map);
 }
